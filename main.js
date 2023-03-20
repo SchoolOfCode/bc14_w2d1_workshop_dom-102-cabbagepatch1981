cbbc1d085826303acaf78df9bb4eb36c98957503
Victoria Burton <vlaburton@gmail.com>
2023-03-20 21:40:49 +0000

    Task 5 complete

--- main.js
+++ main.js
@@ -41,9 +41,23 @@ function handleClick() {
 }
 
 // TASK FIVE CREATING ELEMENTS
-//5.1 USE 
+//5.1 COMPLETE
+//Create a paragraph element using the DOM.
+// - access the body of the document and create a new paragraph
+// - append the new paragraph as a child of the the proof-section id. element 
 
+let newParagraph = document.createElement("p");
+// Write a new paragraph
+newParagraph.textContent = ("You can't lose at penny doubler... just keep going!");
+//Add the new paragraph to the proof-id section here
+document.querySelector("#proof-section").appendChild(newParagraph);
 
+// 5.2 ADD MORE TIPS
+// Use a for loop to create an unordered list with the id of #tips-list 
+// Pass the array to the unordered list
+// create an `li` element
+// set the `textContent` of the `li` element to be the current tip
+// append the newly created element as a child of the `ul`
 
 let additionalTips = [
   "Keep your pennies is a glass jar",
@@ -51,3 +65,14 @@ let additionalTips = [
   "Don't let your partner know...",
 ];
 
+//Setting up a for loop to go through the additionalTips array
+for (let i = 0; i < additionalTips.length; i++) {
+//create newTipsList variable in document.  Create a <li>.
+  let newTipsList = document.createElement("li");
+//newTipsList goes to look for text content inside the additionalTips array and newTipsList takes text
+//content from additionalTips array and runs a loop until it finishes on the length of the additional tips.
+  newTipsList.textContent = additionalTips[i];
+//Go back into the document, find the tips-list element and append the newTipsList.
+//Adds to the browser additional tips for success to the browser
+  document.querySelector("#tips-list").appendChild(newTipsList);
+}
